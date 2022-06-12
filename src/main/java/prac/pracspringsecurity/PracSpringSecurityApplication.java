4b06f32a1c167788207129f3a947386ba417842a
orca <hee1su123@naver.com>
2022-06-12 16:19:44 +0900

    Initial commit

---
+++ src/main/java/prac/pracspringsecurity/PracSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package prac.pracspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracSpringSecurityApplication.class, args);
+    }
+
+}
